730284cf887fe8b62114b3b6f5447f33b221e5a5
ummulkhairabdulkarim <ummalabdul@yahoo.com>
2024-04-02 16:06:22 +0100

    Initial Commit

---
+++ xOApp/xOAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  xOAppApp.swift
+//  xOApp
+//
+//  Created by Ummul Khair Abdulkarim on 02/04/2024.
+//
+
+import SwiftUI
+
+@main
+struct xOAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ xOAppUITests/xOAppUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  xOAppUITestsLaunchTests.swift
+//  xOAppUITests
+//
+//  Created by Ummul Khair Abdulkarim on 02/04/2024.
+//
+
+import XCTest
+
+final class xOAppUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
